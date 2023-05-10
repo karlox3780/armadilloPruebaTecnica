f02a970a5960f91e6b5ad5444a7b80e9aa646c5d
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-05-10 15:09:26 +0200

    Solucionado error en las URL del service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { MoviesService } from 'src/services/movies.service';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDetailComponent,
+    MoviesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgbModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [MoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
